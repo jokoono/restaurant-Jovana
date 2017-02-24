69c847899b427e4724440a2cd514349ea41f860d
jokoono <jovanajovsic@yahoo.com>
2017-02-24 14:36:35 +0100

    Kreiranje tab sekcije

--- restaurant-Jovana/public_html/js/main.js
+++ restaurant-Jovana/public_html/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-    $('.search').click(function () {
+    $('.search').click(function (e) {
+        e.preventDefault();
         $('.form-group').toggle();
 
     });
