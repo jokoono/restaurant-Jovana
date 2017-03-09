8a52d81d7c46939f421654bad27bc58ab83c2451
jokoono <jovanajovsic@yahoo.com>
2017-03-09 10:48:56 +0100

    Blog list strana pocetak

--- restaurant-Jovana/public_html/js/main.js
+++ restaurant-Jovana/public_html/js/main.js
@@ -183,31 +183,33 @@ $(document).ready(function () {
         slidesToShow: 1,
         adaptiveHeight: true
     });
-    
+
     $('.form_date').datetimepicker({
-    
+
         weekStart: 1,
-        todayBtn:  1,
-		autoclose: 1,
-		todayHighlight: 1,
-		startView: 2,
-		minView: 2,
-		forceParse: 0
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
     });
-	$('.form_time').datetimepicker({
-        language:  'fr',
+    $('.form_time').datetimepicker({
+        language: 'fr',
         weekStart: 1,
-        todayBtn:  1,
-		autoclose: 1,
-		todayHighlight: 1,
-		startView: 1,
-		minView: 0,
-		maxView: 1,
-		forceParse: 0
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 1,
+        minView: 0,
+        maxView: 1,
+        forceParse: 0
     });
-    
-    
-    
+
+
+   
+
+
 
 
 
