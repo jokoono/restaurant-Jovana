838e7c460f954e12f662949d19ae3b81fe298546
jokoono <jovanajovsic@yahoo.com>
2017-03-16 16:06:58 +0100

    Shop product cart

--- restaurant-Jovana/public_html/js/main.js
+++ restaurant-Jovana/public_html/js/main.js
@@ -1,82 +1,66 @@
 $(document).ready(function () {
 
-    
+
     $('.search').click(function (e) {
         e.preventDefault();
         $('.search .form-group').toggle();
 
     });
 
-    $('.core-slider').coreSlider({
+    if ($('.core-slider').length > 0) {
+        $('.core-slider').coreSlider({
+            // autoplay interval
+            interval: 5000,
+            // infinite loop
+            loop: true,
+            // enable autoplay
+            slideshow: false,
+            // enable auto resize depending on the screen size
+            resize: true,
+            // pause on hover
+            pauseOnHover: true,
+            // start on hover
+            startOnHover: false,
+            // slider selectors
+            sliderSelector: '.core-slider_list',
+            viewportSelector: '.core-slider_viewport',
+            itemSelector: '.core-slider_item',
+            // enable nav arrows
+            navEnabled: false,
+            // selectors for nav arrows
+            navSelector: '.core-slider_nav',
+            navItemNextSelector: '.core-slider_arrow__right',
+            navItemPrevSelector: '.core-slider_arrow__left',
+            // enable nav dots
+            controlNavEnabled: true,
+            // selectors for nav dots
+            controlNavSelector: '.core-slider_control-nav',
+            controlNavItemSelector: 'core-slider_control-nav-item',
+            // will be added when slider is fully loaded
+            loadedClass: 'is-loaded',
+            // will be added to cloned slides (see option 'clone')
+            clonedClass: 'is-cloned',
+            // will be added when item is disabled (in most of cases - item will be display: noned)
+            disabledClass: 'is-disabled',
+            // will be added to active items (for example control navs, etc.)
+            activeClass: 'is-active',
+            // Reload gif's on slide change for replaying cycled animation inside current slide
+            reloadGif: false,
+            // Indicates, that at begin and at end of slider carousel items will be cloned to create 'infitite' carousel illusion
+            clone: false,
+            // How mutch items will be placed inside viewport. 
+            // Leave 1 if this is slider, 2 ot more - it will look like a carousel                                           
+            items: 1,
+            // How many items must be slided by one action (NOTE: Must be less than 'items' option)                             
+            itemsPerSlide: 1,
+            // How mutch items will be cloned at begin and at end of slider                    
+            cloneItems: 0
 
-        // autoplay interval
-        interval: 5000,
+        });
+    }
 
-        // infinite loop
-        loop: true,
 
-        // enable autoplay
-        slideshow: false,
 
-        // enable auto resize depending on the screen size
-        resize: true,
-
-        // pause on hover
-        pauseOnHover: true,
-
-        // start on hover
-        startOnHover: false,
-
-        // slider selectors
-        sliderSelector: '.core-slider_list',
-        viewportSelector: '.core-slider_viewport',
-        itemSelector: '.core-slider_item',
-
-        // enable nav arrows
-        navEnabled: false,
-
-        // selectors for nav arrows
-        navSelector: '.core-slider_nav',
-        navItemNextSelector: '.core-slider_arrow__right',
-        navItemPrevSelector: '.core-slider_arrow__left',
-
-        // enable nav dots
-        controlNavEnabled: true,
-
-        // selectors for nav dots
-        controlNavSelector: '.core-slider_control-nav',
-        controlNavItemSelector: 'core-slider_control-nav-item',
-
-        // will be added when slider is fully loaded
-        loadedClass: 'is-loaded',
-
-        // will be added to cloned slides (see option 'clone')
-        clonedClass: 'is-cloned',
-
-        // will be added when item is disabled (in most of cases - item will be display: noned)
-        disabledClass: 'is-disabled',
-
-        // will be added to active items (for example control navs, etc.)
-        activeClass: 'is-active',
-
-        // Reload gif's on slide change for replaying cycled animation inside current slide
-        reloadGif: false,
-
-        // Indicates, that at begin and at end of slider carousel items will be cloned to create 'infitite' carousel illusion
-        clone: false,
-
-        // How mutch items will be placed inside viewport. 
-        // Leave 1 if this is slider, 2 ot more - it will look like a carousel                                           
-        items: 1,
-
-        // How many items must be slided by one action (NOTE: Must be less than 'items' option)                             
-        itemsPerSlide: 1,
-
-        // How mutch items will be cloned at begin and at end of slider                    
-        cloneItems: 0
-
-    });
-    
 
 
     function showSlider(a) {
@@ -116,8 +100,10 @@ $(document).ready(function () {
             ]
         });
     }
-
-    showSlider('.breakfast-slide');
+if($('.breakfast-slide').length>0){
+     showSlider('.breakfast-slide');
+}
+   
 
     $('#bfas-tab').click(function () {
         console.log(this);
@@ -177,40 +163,50 @@ $(document).ready(function () {
     });
 
 
-    $('.people-slide').slick({
+    if ($('.people-slide').length > 0) {
+        $('.people-slide').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true
+        });
+    }
 
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
+    if ($('.form_date').length > 0) {
+        $('.form_date').datetimepicker({
+            weekStart: 1,
+            todayBtn: 1,
+            autoclose: 1,
+            todayHighlight: 1,
+            startView: 2,
+            minView: 2,
+            forceParse: 0
+        });
+    }
+
+
+    if ($('.form_time').length > 0) {
+        $('.form_time').datetimepicker({
+            language: 'fr',
+            weekStart: 1,
+            todayBtn: 1,
+            autoclose: 1,
+            todayHighlight: 1,
+            startView: 1,
+            minView: 0,
+            maxView: 1,
+            forceParse: 0
+        });
+    }
+
+
+
+    $('a.favorite').click(function (e) {
+        e.preventDefault();
+        $(this).find('span').toggleClass('fa-heart-o fa-heart');
     });
 
-    $('.form_date').datetimepicker({
-
-        weekStart: 1,
-        todayBtn: 1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
-    $('.form_time').datetimepicker({
-        language: 'fr',
-        weekStart: 1,
-        todayBtn: 1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 1,
-        minView: 0,
-        maxView: 1,
-        forceParse: 0
-    });
-
-
-   
-  
 
 
 
